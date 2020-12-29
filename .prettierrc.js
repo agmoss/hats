7ee3d96122d1a2fcc5dfefbb320106c171234b85
Andrew Moss <andrew@m0ss.dev>
2020-12-29 13:24:52 -0700

    moving to monorepo

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,8 @@
 module.exports = {
-  semi: true,
-  trailingComma: "es5",
-  tabWidth: 4,
-  singleQuote: false,
-  useTabs: false,
+    semi: true,
+    trailingComma: "es5",
+    tabWidth: 4,
+    singleQuote: false,
+    useTabs: false,
+    endOfLine: "auto",
 };
