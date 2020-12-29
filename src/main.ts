e157263f9503fdefc190c8fcf5e2f58c988c317d
Andrew Moss <andrew@m0ss.dev>
2020-12-29 10:16:19 -0700

    sequelize configs for database connection

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,26 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
+
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
+
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
 
     await app.listen(3000);
     console.log(`Application is running on: ${await app.getUrl()}`);
 }
-bootstrap();
+
+if (!module.parent) {
+    console.log("Starting bootstrap");
+    try {
+        bootstrap().catch((e) => {
+            console.error(`Bootstrap Error => ${e}`);
+        });
+    } catch (e) {
+        console.error(e);
+    }
+}
