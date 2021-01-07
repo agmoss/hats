9bf40b154e3610ddd25e9934c0c23c54ffe4f4e5
Andrew Moss <andrew@m0ss.dev>
2021-01-07 12:18:18 -0700

    fixing sequelize config

--- packages/express-graphql-sequelize/src/graphql.schema.ts
+++ packages/express-graphql-sequelize/src/graphql.schema.ts
@@ -6,37 +6,39 @@
 
 /* tslint:disable */
 /* eslint-disable */
-export interface CreateCatInput {
+export class CreateCatInput {
     name?: string;
     age?: number;
 }
 
-export interface PageParams {
+export class PageParams {
     limit: number;
     offset: number;
 }
 
-export interface IQuery {
-    getCats(): Cat[] | Promise<Cat[]>;
-    pages(params?: PageParams): CatConnection | Promise<CatConnection>;
-    cat(id: string): Cat | Promise<Cat>;
+export abstract class IQuery {
+    abstract getCats(): Cat[] | Promise<Cat[]>;
+
+    abstract pages(params?: PageParams): CatConnection | Promise<CatConnection>;
+
+    abstract cat(id: string): Cat | Promise<Cat>;
 }
 
-export interface IMutation {
-    createCat(createCatInput?: CreateCatInput): Cat | Promise<Cat>;
+export abstract class IMutation {
+    abstract createCat(createCatInput?: CreateCatInput): Cat | Promise<Cat>;
 }
 
-export interface ISubscription {
-    catCreated(): Cat | Promise<Cat>;
+export abstract class ISubscription {
+    abstract catCreated(): Cat | Promise<Cat>;
 }
 
-export interface Cat {
+export class Cat {
     id?: number;
     name?: string;
     age?: number;
 }
 
-export interface CatConnection {
+export class CatConnection {
     totalCount: number;
     cats?: Cat[];
 }
